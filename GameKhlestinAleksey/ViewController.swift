e3ceaedfe0700edd2397b7f8dc1376661349399c
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-10-14 11:33:33 +0300

    Initial Commit

---
+++ GameKhlestinAleksey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameKhlestinAleksey
+//
+//  Created by 1234 on 14.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
